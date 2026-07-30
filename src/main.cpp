9aaec6910d469107ae9ab0bc12a09fe7fb32c0d5
sahibahmed <sahib.ahmed006@gmail.com>
2026-07-30 13:59:09 -0400

    Implement manual joystick control for base joint

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+const int x = A0; //arduino connection pins
+const int y = A1; 
+int baseAngle = 90; //use this to remember base servo position
+
+#define SERVOMIN 150
+#define SERVOMAX 600
+
+void setServoAngle(int channel, int angle)
+{
+  angle = constrain(angle, 0, 180);
+  int pulse = map(angle, 0, 180, SERVOMIN, SERVOMAX);
+  pwm.setPWM(channel, 0, pulse);
+} //convert pulse values into readable angle values
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    pwm.begin();
+    pwm.setPWMFreq(50);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() { //velocity-style control
+    int x_val = analogRead(x); //read joystick x value
+    int y_val = analogRead(y);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+    if (x_val < 450){ //left --> dec servo angle(go left)
+      baseAngle--; 
+    }
+    else if (x_val > 574){//right --> inc servo angle(go right)
+      baseAngle++; 
+    }
+
+    baseAngle = constrain(baseAngle, 40, 140); //constraint
+
+    setServoAngle(0, baseAngle); //move servo to stroed position
+
+    delay(15);
+
 }
