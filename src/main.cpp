808a173fe4c9d64c780f069ff570ab329ec00cd2
sahibahmed <sahib.ahmed006@gmail.com>
2026-08-03 00:40:13 -0400

    Implemented manual velocity-based joystick control for base and shoulder

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,10 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-const int x = A0; //arduino connection pins
-const int y = A1; 
+const int x_1 = A0; //arduino connection pins
+const int y_1 = A1; 
 int baseAngle = 90; //use this to remember base servo position
+int shoulderAngle = 130;
 
 #define SERVOMIN 150
 #define SERVOMAX 600
@@ -24,21 +25,31 @@ void setup() {
 }
 
 void loop() { //velocity-style control
-    int x_val = analogRead(x); //read joystick x value
-    int y_val = analogRead(y);
+    int x_val = analogRead(x_1); //read joystick x value
+    int y_val = analogRead(y_1);
 
 
-    if (x_val < 450){ //left --> dec servo angle(go left)
+    if (x_val < 450)
+    {                      //left --> dec servo angle(go left)
       baseAngle--; 
     }
-    else if (x_val > 574){//right --> inc servo angle(go right)
+    else if (x_val > 574){ //right --> inc servo angle(go right)
       baseAngle++; 
     }
 
-    baseAngle = constrain(baseAngle, 40, 140); //constraint
+    if (y_val < 450)
+    {
+      shoulderAngle--;
+    }
+    else if (y_val > 574){
+      shoulderAngle++;
+    }
+
+    baseAngle = constrain(baseAngle, 40, 140); //constraint for base joint
+    shoulderAngle = constrain(shoulderAngle, 40, 150); //constraint for shoulder joint
 
     setServoAngle(0, baseAngle); //move servo to stroed position
+    setServoAngle(1, shoulderAngle); 
 
     delay(15);
-
 }
